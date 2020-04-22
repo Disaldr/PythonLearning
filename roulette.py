2eec75b2982e752597922b4e8f5128b8aabdeb37
Lenamikela <yelenamikeladze@gmail.com@>
2020-04-22 21:44:44 +0300

    game2

--- roulette.py
+++ roulette.py
@@ -1,9 +1,11 @@
 import random
 
-bullets = [0 for i in range(6)]
-bullets[0] = 1
+num = int(input("Под какое количество патронов обойма?: "))
+real_bullets = int(input("Сколько патронов в обойме?: "))
+bullets = [0 for i in range(num-real_bullets)]
+bullets += [1 for j in range(real_bullets)]
 
-a = int(input("Хочешь сыграть? Если да, то напиши 1, а если нет, то 0: "))
+a = int(input("Точно хочешь сыграть? Если да, то напиши 1, а если нет, то 0: "))
 
 while a == 1:
     if random.choice(bullets) == 1:
