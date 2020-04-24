275383b389ac0514b5a2768bd5e98b12f75eb523
Lenamikela <yelenamikeladze@gmail.com@>
2020-04-24 21:26:19 +0300

    start

---
+++ TranslateGame.py
@@ -0,0 +1,13 @@
+print("Приветствую в игре Транслятор! Введи слова и их перевод, а я проведу тестирование по этим словам")
+dictionary = {}
+
+# Цикл для заполнения словаря
+while True:
+    key = input("Введите слово на аглийской или Stop: ").title()
+    if key == "stop":
+        break
+
+        value = input("Введите его перевод или Stop: ").title()
+        if value == "Stop"
+            break
+        dictionary[key] = value
